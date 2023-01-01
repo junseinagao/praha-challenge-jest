991ab3433eb16ad04a80ecc9c93e9ee68cccd452
junseinagao <junsei.nagao@gmail.com>
2023-01-01 18:30:57 +0900

    fix: 🐛 空の配列を渡した時にエラーになるバグを修正

--- __tests__/functions.test.ts
+++ __tests__/functions.test.ts
@@ -15,15 +15,6 @@ describe("sumOfArray", () => {
   it("配列の要素が一つのとき合計を正しく返す", () => {
     expect(sumOfArray([1])).toBe(1);
   });
-  it("空の配列のときTypeErrorをthrowする", () => {
-    expect(() => sumOfArray([])).toThrow(TypeError);
-  });
-  it("Array<number>以外の配列のとき未定義の動作をする", () => {
-    // MEMO: テストを実行するために @ts-ignore を許容している
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    expect(sumOfArray(Array.from("あいうえお"))).toBe("あいうえお");
-  });
 });
 
 describe("asyncSumOfArray", () => {
@@ -32,9 +23,6 @@ describe("asyncSumOfArray", () => {
   });
   it("配列の要素の合計を正しく返す(asyncを使わずに書いた場合)", () => {
     return expect(asyncSumOfArray([1, 2, 3])).resolves.toBe(6);
-  });
-  it("空の配列のときTypeErrorをthrowする", () => {
-    return expect(asyncSumOfArray([])).rejects.toThrow(TypeError);
   });
 });
 
--- functions.ts
+++ functions.ts
@@ -2,7 +2,7 @@ import { NameApiService } from "./nameApiService";
 import { DatabaseMock } from "./util";
 
 export const sumOfArray = (numbers: number[]): number => {
-  return numbers.reduce((a: number, b: number): number => a + b);
+  return numbers.reduce((a: number, b: number): number => a + b, 0);
 };
 
 export const asyncSumOfArray = (numbers: number[]): Promise<number> => {
